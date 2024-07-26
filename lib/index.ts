3a989e71cda2e10ad092814c0095022a102d6b78
Janak <janak2@users.noreply.github.com>
2024-07-26 13:51:00 -0700

    udpate sentry config

--- lib/index.ts
+++ lib/index.ts
@@ -7,12 +7,18 @@ Sentry.init({
     Sentry.replayIntegration(),
   ],
   // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  tracesSampleRate: 0.25, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  tracePropagationTargets: [/^https:\/\/infra\.getadapt\.ai\/run/],
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  beforeSend: (event) => {
+    if (window.location.hostname === "localhost") {
+      return null;
+    }
+    return event;
+  },
 });
 
 import { useConfig } from "./hooks/useConfig";
