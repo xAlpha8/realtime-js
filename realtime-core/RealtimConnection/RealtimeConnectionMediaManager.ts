03b28c8d113772766324c65ed3429a066be2bb78
Ankit Kumar <ankit.kumar@getadapt.ai>
2024-08-10 17:17:19 +0900

    update media manager

--- realtime-core/RealtimConnection/RealtimeConnectionMediaManager.ts
+++ realtime-core/RealtimConnection/RealtimeConnectionMediaManager.ts
@@ -1,45 +1,100 @@
+import { TLogger, TMedia, TRealtimeConfig } from "../shared/@types";
+
 /**
  * This class handles getting access of all the media devices after reading the config
  */
 export class RealtimeConnectionMediaManager {
-  private peerConnection: RTCPeerConnection;
+  private readonly _config: TRealtimeConfig;
+  private _logger: TLogger | undefined;
+  private _localAudio: TMedia | null = null;
+  private _localVideo: TMedia | null = null;
+  private _peerConnection: RTCPeerConnection;
+  private readonly _logLabel = "RealtimeConnectionMediaManager";
 
-  constructor(peerConnection: RTCPeerConnection) {
-    this.peerConnection = peerConnection;
+  constructor(peerConnection: RTCPeerConnection, config: TRealtimeConfig) {
+    this._peerConnection = peerConnection;
+    this._config = config;
+    this._logger = this._config.logger;
   }
-  /**
-   * Adds track to peerConnection.
-   */
-  async addTracksToPeerConnection() {}
 
   /**
-   * Gets all the required media access by reading the config
-   * and returns all the media stream.
-   *
-   * @returns {Promise<MediaStream[]>} - Resolves when media access is obtained.
-   * @throws Will throw an error if media access cannot be obtained.
+   * Read the config, build the media stream constraints and
+   * display media constraints and add save streams.
    */
-  async getMediaStreams(): Promise<MediaStream[]> {}
+  async setup(): Promise<boolean> {
+    const constraints: MediaStreamConstraints = {};
 
-  /**
-   * Gets user media access (e.g., camera and microphone).
-   
-   * @param {MediaTrackConstraints} constaints - Audio or video constraints.
-   * @returns {Promise<MediaStream>} - Resolves with the MediaStream object if access is granted.
-   * @throws Will throw an error if user media access cannot be obtained.
-   */
-  async getUserMediaAccess(
-    constraints: MediaTrackConstraints
-  ): Promise<MediaStream> {}
+    const audioConfig = this._config.audio;
+    const videoConfig = this._config.video;
+    // const screenConfig = this._config.screen;
 
-  /**
-   * Gets user display access (e.g., screen sharing).
-   *
-   * @param {DisplayMediaStreamOptions} constaints - Screen share constraints.
-   * @returns {Promise<MediaStream>} - Resolves with the MediaStream object if access is granted.
-   * @throws Will throw an error if user display access cannot be obtained.
-   */
-  async getUserDisplayAccess(
-    constraints: DisplayMediaStreamOptions
-  ): Promise<MediaStream> {}
+    if (audioConfig) {
+      constraints.audio =
+        typeof audioConfig === "boolean"
+          ? audioConfig
+          : audioConfig.constraints;
+    }
+
+    if (videoConfig) {
+      constraints.video =
+        typeof videoConfig === "boolean"
+          ? videoConfig
+          : videoConfig.constraints;
+    }
+
+    const mediaStream = await this.getUserMedia(constraints);
+
+    if (!mediaStream) {
+      this._logger?.warn(this._logLabel, "Unable to get media stream");
+      return false;
+    }
+
+    mediaStream.getTracks().forEach((track) => {
+      if (track.kind.includes("audio")) {
+        this._localAudio = {
+          track,
+          stream: mediaStream,
+        };
+      } else if (track.kind.includes("video")) {
+        this._localVideo = {
+          track,
+          stream: mediaStream,
+        };
+      }
+    });
+
+    if (audioConfig && !this._localAudio) {
+      this._logger?.warn(this._logLabel, "Unable to get audio stream.");
+    }
+
+    if (videoConfig && !this._localVideo) {
+      this._logger?.warn(this._logLabel, "Unable to get video stream.");
+    }
+
+    return true;
+  }
+
+  async getUserMedia(constraints: MediaStreamConstraints) {
+    try {
+      return await navigator.mediaDevices.getUserMedia(constraints);
+    } catch (e) {
+      this._logger?.error(this._logLabel, e);
+      return null;
+    }
+  }
+
+  getLocalVideoStream() {
+    if (this._localVideo) {
+      return this._localVideo.stream;
+    }
+    return null;
+  }
+
+  getLocalAudioStream() {
+    if (this._localAudio) {
+      return this._localAudio.stream;
+    }
+
+    return null;
+  }
 }
