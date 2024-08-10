996d3a6e442712214186cdcf029b3fdfa09b5b5d
Ankit Kumar <ankit.kumar@getadapt.ai>
2024-08-10 17:15:25 +0900

    add stun servers

--- realtime-core/constants.ts
+++ realtime-core/constants.ts
@@ -1,4 +1,8 @@
-export const ICE_STUN_SERVERS: RTCIceServer[] = [];
+export const ICE_STUN_SERVERS: RTCIceServer[] = [
+  { urls: "stun:stun.l.google.com:19302" },
+  { urls: "stun:stun2.l.google.com:5349" },
+  { urls: "stun:stun4.l.google.com:5349" },
+];
 export const DEFAULT_VIDEO_CONSTRAINTS: MediaTrackConstraints = {};
 export const DEFAULT_AUDIO_CONSTRAINTS: MediaTrackConstraints = {};
 export const DEFAULT_SCREEN_CONSTRAINTS: DisplayMediaStreamOptions = {};
