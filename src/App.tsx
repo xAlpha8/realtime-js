9e6647f04e0211c42ae66277952cb0d5d1694c5d
Ankit Kumar <ankit.kumar@getadapt.ai>
2024-08-19 08:40:51 +0900

    update demo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,132 +1,19 @@
-import { useState, useEffect } from "react";
+import React from "react";
+import { TakeUserInput } from "./TakeUserInput";
+import { TRealtimeConfig } from "../realtime-core/shared/@types";
+import { RealtimeApp } from "./RealtimeApp";
+
 import "./App.css";
-import { useConfig, useRealtime } from "../lib";
-import { Config } from "../lib";
-import { RtVideo, RtAudio, RtChat } from "../lib";
-import { RtAudioVisualizer } from "../lib/components/rtaudiovisualizer";
 
-function RealtimeContainer({ config }: { config: Config }) {
-  const { connection, isConnected } = useRealtime(config);
-  useEffect(() => {
-    connection.connect();
-  }, []);
-
-  useEffect(() => {
-    if (isConnected) {
-      connection.onAudioPacketReceived((timestamp, prevTimestamp, source) => {
-        console.log(timestamp, prevTimestamp, source);
-      }, 10000);
-    }
-  }, [connection, isConnected]);
-
-  return (
-    <div>
-      {isConnected ? (
-        <>
-          <RtVideo rtConnection={connection} />
-          <RtAudio rtConnection={connection} />
-          <RtAudioVisualizer
-            rtConnection={connection}
-            height={100}
-            width={100}
-          />
-          <RtChat rtConnection={connection} />
-        </>
-      ) : (
-        <>Connecting!</>
-      )}
-    </div>
-  );
-}
-
-function App() {
-  const [isRealtimeDisabled, setIsRealtimeDisabled] = useState(true);
-  const [config, setConfig] = useState<Config | null>(null);
-  const configDefault: Config = {
-    functionUrl: "",
-    offerUrl: "http://0.0.0.0:8080/offer",
-    isDataEnabled: true,
-    dataParameters: { ordered: true },
-    isVideoEnabled: true,
-    videoInput: "",
-    videoCodec: "default",
-    videoResolution: "256x256",
-    videoTransform: "none",
-    isScreenShareEnabled: false,
-    isAudioEnabled: true,
-    audioInput: "",
-    audioCodec: "PCMU/8000",
-    useStun: false,
-  };
-
-  const { options, setters, values, dump } = useConfig(configDefault);
-  const { audioOptions, videoOptions } = options;
-  const { setAudioInput, setVideoInput, setOfferUrl, setFunctionUrl } = setters;
-  const { audioInput, videoInput, offerUrl, functionUrl } = values;
-
-  const dumpConfigAndRun = () => {
-    const configDump = dump();
-    setConfig(configDump);
-    setIsRealtimeDisabled(false);
-  };
+export default function App() {
+  const [config, setConfig] = React.useState<TRealtimeConfig | null>(null);
 
   return (
     <>
-      {isRealtimeDisabled ? (
-        <div>
-          <div>
-            <h2>Audio Options:</h2>
-            <select
-              value={audioInput}
-              onChange={(e) => setAudioInput(e.target.value)}
-            >
-              {audioOptions.map((option, index) => (
-                <option key={index} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <h2>Video Options:</h2>
-            <select
-              value={videoInput}
-              onChange={(e) => setVideoInput(e.target.value)}
-            >
-              {videoOptions.map((option, index) => (
-                <option key={index} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <h2>Offer URL:</h2>
-            <input
-              type="text"
-              value={offerUrl}
-              onChange={(e) => setOfferUrl(e.target.value)}
-              placeholder="Enter Offer URL"
-            />
-          </div>
-          <div>
-            <h2>Function URL:</h2>
-            <input
-              type="text"
-              value={functionUrl}
-              onChange={(e) => setFunctionUrl(e.target.value)}
-              placeholder="Enter Function URL"
-            />
-          </div>
-          <div>
-            <button onClick={dumpConfigAndRun}>Run</button>
-          </div>
-        </div>
-      ) : (
-        <div>{config && <RealtimeContainer config={config} />}</div>
+      {!config && <TakeUserInput onSubmit={(data) => setConfig(data)} />}
+      {config && (
+        <RealtimeApp config={config} onDisconnect={() => setConfig(null)} />
       )}
     </>
   );
 }
-
-export default App;
