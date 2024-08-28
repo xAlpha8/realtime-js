e2d0c927d11b874a2ff406939f8aec96773f8c12
Ankit Kumar <ankit.kumar@getadapt.ai>
2024-08-29 01:50:35 +0900

    update app to use websocket

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from "react";
-import { TakeUserInput } from "./TakeUserInput";
-
-import { RealtimeApp } from "./RealtimeApp";
+import { useAvailableMediaDevices, useWebSocket } from "../realtime-react";
 
 import "./App.css";
-import { TRealtimeConfig } from "../realtime-core";
+import { ConsoleLogger } from "../realtime-core";
+import React from "react";
 
 export default function App() {
-  const [config, setConfig] = React.useState<TRealtimeConfig | null>(null);
+  const [audioDeviceId, setAudioDeviceId] = React.useState("");
+  const { availableAudioDevices } = useAvailableMediaDevices();
 
-  function onSubmit(config: TRealtimeConfig) {
-    setConfig(config);
-  }
-
-  function onDisconnect() {
-    setConfig(null);
-  }
+  const { connect, disconnect } = useWebSocket({
+    config: {
+      audio: {
+        deviceId: audioDeviceId,
+        echoCancellation: true,
+      },
+      functionURL:
+        "https://us0-dev.getadapt.ai/run/eb7bf856460c223978795a96d2cd2073",
+      logger: ConsoleLogger.getLogger(),
+    },
+  });
 
   return (
-    <>
-      {!config && <TakeUserInput onSubmit={onSubmit} />}
-      {config && <RealtimeApp config={config} onDisconnect={onDisconnect} />}
-    </>
+    <div>
+      <select
+        value={audioDeviceId}
+        onChange={(e) => setAudioDeviceId(e.target.value)}
+      >
+        {availableAudioDevices.map((device) => (
+          <option key={device.deviceId} value={device.deviceId}>
+            {device.label || device.deviceId}
+          </option>
+        ))}
+      </select>
+      <button onClick={connect}>Connect</button>
+      <button onClick={disconnect}>Disconnect</button>
+    </div>
   );
 }
