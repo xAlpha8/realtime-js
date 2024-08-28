1dd313f5e06430f7683e5e63020df2d9797e03bc
Ankit Kumar <ankit.kumar@getadapt.ai>
2024-08-29 01:49:38 +0900

    remove unused code

--- realtime-react/hooks/utils.ts
+++ realtime-react/hooks/utils.ts
@@ -1,21 +1,8 @@
-import { snakeCase } from "change-case";
-
 export const blobToBase64 = (blob: Blob): Promise<string | null> => {
-  return new Promise((resolve, _) => {
+  return new Promise((resolve) => {
     const reader = new FileReader();
     reader.onloadend = () =>
       resolve(reader.result?.toString().split(",")[1] || null);
     reader.readAsDataURL(blob);
-  });
-};
-
-export const stringify = (obj: object): string => {
-  return JSON.stringify(obj, (_, value) => {
-    if (value && typeof value === "object") {
-      return Object.fromEntries(
-        Object.entries(value).map(([k, v]) => [snakeCase(k), v])
-      );
-    }
-    return value;
   });
 };
