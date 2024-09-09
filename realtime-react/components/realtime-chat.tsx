38672c6dc98b3a46d705b1f24adb4a8a85c9d186
Ankit Kumar <ankit.kumar@getadapt.ai>
2024-09-09 22:37:07 +0900

    add chat component

--- realtime-react/components/realtime-chat.tsx
+++ realtime-react/components/realtime-chat.tsx
@@ -1,7 +1,6 @@
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-import { isMessageEvent } from "../../realtime-core/utils";
-import { DataChannel } from "../../realtime-core";
+import { DataChannel, isMessageEvent } from "../../realtime-core";
 
 export type RealtimeChatProps = {
   dataChannel: DataChannel<unknown>;
@@ -11,9 +10,49 @@ export function RealtimeChat(props: RealtimeChatProps) {
   const { dataChannel } = props;
   const chatRef = useRef<HTMLAudioElement>(null);
   const [messages, setMessages] = useState<
-    { content?: string; text?: string }[]
+    { content?: string; text?: string; type: "user" | "bot" }[]
   >([]);
   const input = useRef<HTMLInputElement>(null);
+
+  function updateMessage(message: {
+    content?: string;
+    text?: string;
+    type: "user" | "bot";
+  }) {
+    setMessages((currentMessages) => [...currentMessages, message]);
+
+    setTimeout(() => {
+      chatRef.current?.scroll({
+        top: chatRef.current?.scrollHeight,
+        behavior: "smooth",
+      });
+    }, 300);
+  }
+
+  function sendMessage(msg: string) {
+    updateMessage({ content: msg, type: "user" });
+
+    dataChannel.send({
+      content: msg,
+      role: "user",
+    });
+  }
+
+  const fixChatContainerHeight = React.useCallback(() => {
+    const chatContainer = document.getElementById("chat");
+    if (chatContainer) {
+      chatContainer.style.height = `${window.innerHeight - 100}px`;
+    }
+  }, []);
+
+  useEffect(() => {
+    fixChatContainerHeight();
+    window.addEventListener("resize", fixChatContainerHeight);
+
+    return () => {
+      window.removeEventListener("resize", fixChatContainerHeight);
+    };
+  }, [fixChatContainerHeight]);
 
   useEffect(() => {
     const onMessage = (evt: unknown) => {
@@ -27,7 +66,15 @@ export function RealtimeChat(props: RealtimeChatProps) {
 
       try {
         const message = JSON.parse(evt.data);
-        setMessages((currentMessages) => [...currentMessages, message]);
+
+        if (message.widget) {
+          // This is a widget.
+          console.log(
+            <div dangerouslySetInnerHTML={{ __html: message.widget }} />
+          );
+        }
+
+        updateMessage({ ...message, type: "bot" });
       } catch (error) {
         console.error(error);
       }
@@ -40,30 +87,54 @@ export function RealtimeChat(props: RealtimeChatProps) {
   }, [dataChannel]);
 
   return (
-    <div className="rt-chat-container">
-      <section ref={chatRef} className="rt-chat-messages-container">
+    <div
+      id="chat"
+      className="flex-1 h-full flex flex-col border rounded overflow-hidden"
+    >
+      <div className="p-2 font-bold border-b">Chat</div>
+      <section
+        ref={chatRef}
+        className="flex-1 flex flex-col overflow-auto space-y-2 px-2"
+      >
         {messages.map((msg, index) => {
           const data = msg.content || msg.text;
           if (!data) return null;
+
+          if (msg.type === "user") {
+            return (
+              <div
+                key={index}
+                className="ml-auto text-right inline-flex flex-col space-y-1 max-w-[200px]"
+              >
+                <span>User</span>
+                <span className=" py-2 px-4 rounded-[8px] bg-secondary text-foreground">
+                  {data}
+                </span>
+              </div>
+            );
+          }
+
           return (
-            <div key={index} className="rt-chat-message">
-              <b>Bot</b> {data}
+            <div
+              key={index}
+              className="mr-auto inline-flex flex-col space-y-1 max-w-[200px]"
+            >
+              <span>Avatar</span>
+              <span className=" py-2 px-4 rounded-[8px] bg-primary text-background">
+                {data}
+              </span>
             </div>
           );
         })}
       </section>
-      <div className="rt-chat-input-container" style={{ marginTop: 10 }}>
+      <div className="overflow-hidden rounded" style={{ marginTop: 10 }}>
         <input
-          className="rt-chat-input"
+          className="px-2 py-4 w-full rounded bg-gray-100 hover:bg-gray-200 focus:bg-gray-50 focus:border-t"
           placeholder="Type a message & hit Enter"
           ref={input}
           onKeyDown={(e) => {
             if (e.key === "Enter" && input.current?.value) {
-              dataChannel.send({
-                content: input.current.value,
-                role: "user",
-              });
-
+              sendMessage(input.current.value);
               input.current.value = "";
             }
           }}
